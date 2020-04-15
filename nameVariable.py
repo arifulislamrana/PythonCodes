dfb91b44a66e58e359f3b2b5e4c3b9aba74796d6
arifulislamrana <ariful.islam0683@gmail.com>
2020-04-16 00:57:48 +0600

    Add files via upload

---
+++ nameVariable.py
@@ -0,0 +1,16 @@
+def nijhum():
+    print('I am nijhum  '+__name__)
+
+
+def zaber():
+    print("I'm Zaber  "+__name__)
+
+
+def main():
+    nijhum()
+    zaber()
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ nameVariable2.py
@@ -0,0 +1,13 @@
+from nameVariable import nijhum
+
+
+def person():
+    print("hello i m a person")
+    nijhum()
+
+
+def main():
+    person()
+
+
+main()
